55c7b6c6c7e7a7de35616747065cd05e56c07c00
Mateusz Blaszczyk <121445191+smaru90@users.noreply.github.com>
2023-02-17 13:58:04 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+//function to display more news on the homepage
+$(document).ready(function () {
+  $(".articlesThumbnail").hide();
+  $(".articlesThumbnail").slice(0, 4).show();
+  $("#loadMore").click(function (e) {
+    // click event for load more
+    e.preventDefault();
+    $(".articlesThumbnail:hidden").slice(0, 4).slideDown();
+    if ($(".articlesThumbnail:hidden").length == 0) {
+      $("#loadMore").text("Brak więcej wydarzeń").fadOut("slow");
+    }
+  });
+});
+function alert() {
+  alert("Prosimy o kontakt telefoniczny");
+}
+
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
