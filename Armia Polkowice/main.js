a8427a0c286741e7888df7ae7f881af2bbd1bd4f
Mateusz Blaszczyk <121445191+smaru90@users.noreply.github.com>
2023-02-18 16:05:51 +0100

    Add files via upload

--- Armia Polkowice/main.js
+++ Armia Polkowice/main.js
@@ -11,9 +11,7 @@ $(document).ready(function () {
     }
   });
 });
-function alert() {
-  alert("Prosimy o kontakt telefoniczny");
-}
+
 
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const navbarLinks = document.getElementsByClassName("navbar-links")[0];
@@ -21,3 +19,18 @@ const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
 });
+
+window.onload = function() {
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+      event.preventDefault();
+      // generate a five digit number for the contact_number variable
+      this.contact_number.value = Math.random() * 100000 | 0;
+      // these IDs from the previous steps
+      emailjs.sendForm('contact_service', 'contact_form', this)
+          .then(function() {
+              console.log('SUCCESS!');
+          }, function(error) {
+              console.log('FAILED...', error);
+          });
+  });
+}
