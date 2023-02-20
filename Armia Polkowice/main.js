c6d2975317e0a1ab6c46e4b798a972ca190afc8e
Mateusz Blaszczyk <121445191+smaru90@users.noreply.github.com>
2023-02-20 20:02:19 +0100

    Add files via upload

--- Armia Polkowice/main.js
+++ Armia Polkowice/main.js
@@ -20,17 +20,5 @@ toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
 });
 
-window.onload = function() {
-  document.getElementById('contact-form').addEventListener('submit', function(event) {
-      event.preventDefault();
-      // generate a five digit number for the contact_number variable
-      this.contact_number.value = Math.random() * 100000 | 0;
-      // these IDs from the previous steps
-      emailjs.sendForm('contact_service', 'contact_form', this)
-          .then(function() {
-              console.log('SUCCESS!');
-          }, function(error) {
-              console.log('FAILED...', error);
-          });
-  });
-}
+
+
